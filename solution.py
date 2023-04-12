058b08e2f0127f0c3e904a49c7213af4ffa686c6
Anna Bachaeva <90902763+annabachaeva@users.noreply.github.com>
2023-04-12 10:45:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,6 @@ def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
 
   score, pval = ztest([x_success, y_success], [x_cnt, y_cnt])
   bool_solution = True
-  print(pval)
 
   if pval < 0.02:
     bool_solution = True
