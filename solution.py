cacb80b423619387a5fb0d916df43c6438931422
Anna Bachaeva <90902763+annabachaeva@users.noreply.github.com>
2023-04-12 10:44:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,15 +5,15 @@ from statsmodels.stats.proportion import proportions_ztest as ztest
 
 chat_id = 409995141 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-  
-  score, pval = ztest([x_success, y_success], [x_cnt, y_cnt], value=0)
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+
+  score, pval = ztest([x_success, y_success], [x_cnt, y_cnt])
+  bool_solution = True
+  print(pval)
 
   if pval < 0.02:
-    return True
-  
+    bool_solution = True
   else:  
-    return False
+    bool_solution = False
+
+  return bool_solution
